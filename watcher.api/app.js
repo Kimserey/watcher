9242543198028f62d867b7572234c963893d3c9b
Kimserey Lam <kimserey.lam@gmail.com>
2016-03-06 21:40:16 +0000

    added gif

--- watcher.api/app.js
+++ watcher.api/app.js
@@ -1,4 +1,6 @@
 ﻿$(function() {
-	$("#main").append("<div>Hello world!!!</div>");
+	$("#main").append("<div>Hi all!</div>");
 	$("#main").append("<div>I am testing the auto-refresh</div>");
+	$("#main").append("<div>using SSE with Suave!!</div>");
+	$("#main").append("<div>:D</div>");
 })
